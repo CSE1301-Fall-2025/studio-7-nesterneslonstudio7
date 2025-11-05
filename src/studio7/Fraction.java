7dc182ab9ad994105db61416f0b220b0f904ce9b
Blake Nelson <b.h.nelson@wustl.edu>
2025-11-05 13:47:40 -0600

    testing done round 17 fingers crossed

--- src/studio7/Fraction.java
+++ src/studio7/Fraction.java
@@ -6,7 +6,7 @@ public class Fraction {
         this.denominator = denominator;
     }
     public Fraction addFraction(Fraction other){
-        return new Fraction(this.numerator + other.numerator, this.denominator + other.denominator);
+        return new Fraction((this.numerator * other.denominator) + (other.numerator * this.denominator), (this.denominator * other.denominator));
     }
     public Fraction multiplyFraction(Fraction other){
         return new Fraction(this.numerator * other.numerator, this.denominator * other.denominator);
@@ -20,7 +20,7 @@ public class Fraction {
         for(int i = denominator; i > 1; i--){
             if(denominator % i == 0 && numerator % i == 0){
                 denominator = denominator/i;
-                denominator = denominator/i;
+                numerator = numerator/i;
             }
         }
         return new Fraction(numerator, denominator);

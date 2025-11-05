bcde57d9da87032f0f58b5761ac215d4a85c99fc
Blake Nelson <b.h.nelson@wustl.edu>
2025-11-05 13:37:41 -0600

    done round 3

---
+++ src/studio7/Fraction.java
@@ -0,0 +1,28 @@
+public class Fraction {
+    int numerator;
+    int denominator;
+    public Fraction(int numerator, int denominator){
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+    public Fraction addFraction(Fraction other){
+        return new Fraction(this.numerator + other.numerator, this.denominator + other.denominator);
+    }
+    public Fraction multiplyFraction(Fraction other){
+        return new Fraction(this.numerator * other.numerator, this.denominator * other.denominator);
+    }
+    public String display(){
+        return numerator + "/" + denominator;
+    }
+    public Fraction simplify(){
+        denominator = this.denominator;
+        numerator = this.numerator;
+        for(int i = denominator; i > 1; i--){
+            if(denominator % i == 0 && numerator % i == 0){
+                denominator = denominator/i;
+                denominator = denominator/i;
+            }
+        }
+        return new Fraction(numerator, denominator);
+    }
+}
